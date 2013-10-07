f00e35807259690b3c84758cbe892fd14940e779
reza babakhani <babakhani.reza@gamil.com>
2013-10-07 19:23:08 +0330

    fix ie 8

--- js/pwt-datepicker.js
+++ js/pwt-datepicker.js
@@ -7,6 +7,8 @@
 // Beta Version 0.2.1
 // Dependency :  Jquery.js , pwt-date.js
  Chnage Log:
+      0.2.2
+          Fix Ie8 
       0.2.1
             Fix manual entry
        0.2.0
@@ -28,6 +30,32 @@
      0.0.3 remove jquery tmpl
 */
 (function ($) {
+          
+Object.keys = Object.keys || (function() {
+          var hasOwnProperty = Object.prototype.hasOwnProperty, hasDontEnumBug = ! {
+                    toString : null
+          }.propertyIsEnumerable("toString"), DontEnums = ['toString', 'toLocaleString', 'valueOf', 'hasOwnProperty', 'isPrototypeOf', 'propertyIsEnumerable', 'constructor'], DontEnumsLength = DontEnums.length;
+
+          return function(o) {
+                    if ( typeof o != "object" && typeof o != "function" || o === null)
+                              throw new TypeError("Object.keys called on a non-object");
+
+                    var result = [];
+                    for (var name in o) {
+                              if (hasOwnProperty.call(o, name))
+                                        result.push(name);
+                    }
+
+                    if (hasDontEnumBug) {
+                              for (var i = 0; i < DontEnumsLength; i++) {
+                                        if (hasOwnProperty.call(o, DontEnums[i]))
+                                                  result.push(DontEnums[i]);
+                              }
+                    }
+
+                    return result;
+          };
+})();
 
 
     $.event.special.textchange = {
