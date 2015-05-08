a799c49a35622657f3c071f4b9764e4f1ee5cc72
reza babakhani <babakhani.reza@gamil.com>
2015-05-08 22:42:04 +0430

    Quick Fix #45

--- src/js/datepicker.js
+++ src/js/datepicker.js
@@ -426,7 +426,10 @@ var ClassDatepicker = {
      * @desc set time of timepicker
      */
     setTime: function () {
-        this.timePicker.setTime(this.state.selected.unixDate);
+        if(this.timePicker.enabled){
+            this.timePicker.setTime(this.state.selected.unixDate);
+        }
+        return this;
     },
 
 
